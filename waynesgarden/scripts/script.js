995e4c37ae3598a593dd3ad332515418c75b07d6
fredosantana <santana.wil@gmail.com>
2017-09-05 20:30:01 -0400

    JS alert added to advise how to view slider as intended

--- waynesgarden/scripts/script.js
+++ waynesgarden/scripts/script.js
@@ -1,1 +1,1 @@
-alert("If you want to view the slider the way it's intended\nthen disable your browser's privacy settings.");
+alert("If you want to view the slider the way it's intended\nthen disable your browser's privacy settings.\n\nTo be fixed soon. Sorry for the inconvenience :( ");
